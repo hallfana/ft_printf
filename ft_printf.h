9c0bdb6a82c0426c6e9911afb4e20659eca5dcba
hallfana <hallfana@proton.me>
2024-11-12 00:25:14 +0100

    auto update

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 20:19:39 by samberna          #+#    #+#             */
-/*   Updated: 2024/11/12 00:18:48 by samberna         ###   ########.fr       */
+/*   Updated: 2024/11/12 00:25:13 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ size_t	ft_strlen(const char *s);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_print_u(unsigned int n);
 int 	nblen_u(long n);
+int		ft_print_x(unsigned int nb);
 
 #endif
