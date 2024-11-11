0c5f829f8de371cf4d5e060fcbb938117e215ece
hallfana <hallfana@proton.me>
2024-11-11 22:27:57 +0100

    auto update

--- ft_print_s.c
+++ ft_print_s.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 20:41:50 by samberna          #+#    #+#             */
-/*   Updated: 2024/11/11 22:27:50 by samberna         ###   ########.fr       */
+/*   Updated: 2024/11/11 22:27:56 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_print_s(char	*str)
 	int	i;
 
 	i = 0;
-	if (!str || ft_strdup(str) == 0)
+	if (!str || ft_strlen(str) == 0)
 		return (0);
 	while (str[i])
 	{
